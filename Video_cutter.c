23809cf9272c5b6578834fa64bd040af1ea9eec5
DeliKhan <deluckshanmurugamoorthy@gmail.com>
2023-06-28 14:42:22 -0400

    fix: #1

--- Video_cutter.c
+++ Video_cutter.c
@@ -4,17 +4,16 @@
 #include <string.h>   //using this to extract time information
 #include <stdlib.h>   // for using free
 
-void time_to_string(char *start_time, int *timings[3]) // converts HH:MM:SS to {HH, MM, SS}
+void extracting_time(char start_time[], int *const timings) // converts HH:MM:SS to {HH, MM, SS}
 {
-    printf("%s", start_time);
     const char delimiter[2] = ":";
     char *token;
     token = strtok(start_time, delimiter);
     int i = 0;
     while (token)
     {
-        printf("\n%d\n", atoi(token));
-        *(timings + i) = atoi(token);
+        // printf("\n%d\n", atoi(token));
+        timings[i] = atoi(token);
         token = strtok(NULL, delimiter);
         i++;
     }
@@ -24,12 +23,12 @@ void time_to_string(char *start_time, int *timings[3]) // converts HH:MM:SS to {
 int main() // gets all user input and executes the other functions
 {
     char file_directory[100]; // I'm assuming your file directory is less than 100 characters long
-    char start_time[7];
+    char start_time[8];
     int timings[3];
     printf("Type path to input video file");
     scanf("%s", file_directory);
     printf("Type the start time of your clipping in HH:MM:SS");
     scanf("%s", start_time);
-    time_to_string(&start_time, &timings);
-    printf("Hours:%d\nMinutes:%d\nSeconds:%d", *(timings), *(timings + 1), *(timings + 2));
+    extracting_time(&start_time, &timings);
+    // printf("Hours:%d\nMinutes:%d\nSeconds:%d\n", *(timings), *(timings + 1), *(timings + 2));
 }
